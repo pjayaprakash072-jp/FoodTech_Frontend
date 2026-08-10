6878b3971a6dceb709cc230bd1924b62aa8a915f
poojarijayaprakash4 <pjayaprakash072@gmail.com>
2026-08-10 09:41:31 +0530

    used the live backend url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,2 +1,2 @@
-export const API_URI = "http://localhost:3000"
-// export const API_URI = "https://foodtech-backend.onrender.com"
+// export const API_URI = "http://localhost:3000"
+export const API_URI = "https://foodtech-backend.onrender.com"
